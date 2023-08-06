ba9a166bf15cd890164791c516fc836287c658df
Maksim Aliantsevish <132204061+fajgd@users.noreply.github.com>
2023-08-06 20:51:11 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+  module.exports = function reverse (n) {
+    let intToString = int.toString();
+
+    let reversedString = intToString
+        .split("")
+        .reverse()
+        .join("");
+
+    let stringToInt = parseInt(reversedString, 10);
+
+    return stringToInt * Math.sign(int);
+}
 }
