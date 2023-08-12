17840cb045f8b05def3c9dabf4f480ecafbfdf0a
Maksim Aliantsevish <132204061+fajgd@users.noreply.github.com>
2023-08-12 13:19:23 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,4 @@
-
 function reverse(number) {
     const reversedStr = number.toString().split('').reverse().join('');
     return parseInt(reversedStr);
 }
-
-console.log(reverse(123));    // Output: 321
-console.log(reverse(233));    // Output: 332
-console.log(reverse(535));    // Output: 535
-console.log(reverse(95034));  // Output: 43059
