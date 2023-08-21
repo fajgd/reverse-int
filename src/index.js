31f7f3d34a2427aaa92c4c150f6ecbd6add6472d
Maksim Aliantsevish <132204061+fajgd@users.noreply.github.com>
2023-08-21 12:17:43 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 function reverse(number) {
+
   module.exports = function reverse (n) {
     let intToString = int.toString();
 
@@ -10,4 +11,5 @@ function reverse(number) {
     let stringToInt = parseInt(reversedString, 10);
 
     return stringToInt * Math.sign(int);
+}
 }
